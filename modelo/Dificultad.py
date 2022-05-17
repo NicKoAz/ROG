daf8ae4609e7eeaba27c1ad28f93bc463558244e
NicKoAz <nicobk.carreno@gmail.com>
2022-05-17 10:52:07 -0400

    renombrar archivos

--- modelo/Dificultad.py
+++ modelo/Dificultad.py
@@ -4,5 +4,11 @@ Created on 17 may 2022
 @author: Nicolas
 '''
 class Dificultad():
-    def __init__ (self, facil, normal, dificil):
+    def __init__ (self):
+        pass
+    def facil (self):
+        pass
+    def normal(self):
+        pass
+    def dificl (self):
         pass
