4a58e33c1b1e3ee88a36a0e70bd8ea28c18867f7
Carolina <carolina.hp251014@gmail.com>
2022-05-17 11:55:00 -0400

    Arreglo de Vista

--- vista/Interfaz.py
+++ vista/Interfaz.py
@@ -8,18 +8,23 @@ import wx
 
 class Interfaz(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, title="Encontrar Los Pares", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(800, 300))
+        wx.Frame.__init__(self, None, title="Encontrar Los Pares", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(350, 350))
         
         self.panel = wx.Panel(self)
         
-        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(10,0),size=(760, 250))
-        self.lbl1 = wx.StaticText(self.box1,-1, 'ENCONTRAR LOS PARES', pos =(285,50))
-        self.btn1 = wx.Button(self.box1, label = "INICIAR EL JUEGO", size=(200, 30), pos=(250, 100))
-        self.btn2 = wx.Button(self.box1, label = "SALIR DEL JUEGO", size=(200, 30), pos=(250, 150))
+        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(10,0),size=(310, 300))
+        self.lbl1 = wx.StaticText(self.box1,-1, 'ENCONTRAR LOS PARES', pos =(85,50))
+        self.btn1 = wx.Button(self.box1, label = "INICIAR EL JUEGO", size=(200, 50), pos=(50, 100))
+        self.btn2 = wx.Button(self.box1, label = "SALIR DEL JUEGO", size=(200, 50), pos=(50, 170))
+        
+        #Color Letra Botones
+        self.btn1.SetForegroundColour((3, 152, 252, 255))
+        self.btn2.SetForegroundColour((3, 152, 252, 255)) 
 
         # Eventos de Botones
         self.btn1.Bind(wx.EVT_BUTTON, self.StarGame)
         self.btn2.Bind(wx.EVT_BUTTON, self.FinishGame)
+
     
     def StarGame(self,i):
         self.Hide()
@@ -33,14 +38,18 @@ class Interfaz(wx.Frame):
 
 class VentanaModoDeJuego(wx.Dialog):
     def __init__ (self, parent):
-        wx.Dialog.__init__(self, parent, wx.NewId(), title = "Modos De Juego", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(800, 300))
+        wx.Dialog.__init__(self, parent, wx.NewId(), title = "Modos De Juego", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(350, 350))
         
         self.panel = wx.Panel(self)
         
-        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(10,0),size=(760, 250))
-        self.lbl1 = wx.StaticText(self.box1, -1, 'MODOS DE JUEGO', pos =(285,50))
-        self.btn1 = wx.Button(self.box1, label = "CONTRARELOJ", size=(200, 30), pos=(250, 100))
-        self.btn2 = wx.Button(self.box1, label = "MODO LIBRE", size=(200, 30), pos=(250, 150))
+        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(10,0),size=(310, 300))
+        self.lbl1 = wx.StaticText(self.box1, -1, 'MODOS DE JUEGO', pos =(100,50))
+        self.btn1 = wx.Button(self.box1, label = "CONTRARELOJ", size=(200, 50), pos=(50, 100))
+        self.btn2 = wx.Button(self.box1, label = "MODO LIBRE", size=(200, 50), pos=(50, 170))
+        
+        #Color Letra Botones
+        self.btn1.SetForegroundColour((3, 152, 252, 255))
+        self.btn2.SetForegroundColour((3, 152, 252, 255)) 
         
         # Eventos de Botones
         self.btn1.Bind(wx.EVT_BUTTON, self.ModeContraReloj)
@@ -60,15 +69,20 @@ class VentanaModoDeJuego(wx.Dialog):
 
 class VentanaNivelDificultad(wx.Dialog):
     def __init__ (self, parent):
-        wx.Dialog.__init__(self, parent, wx.NewId(), title = "Nivel De Dificultad", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(800, 300))
+        wx.Dialog.__init__(self, parent, wx.NewId(), title = "Nivel De Dificultad", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(350, 350))
         
         self.panel = wx.Panel(self)
         
-        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(10,0),size=(760, 250))
-        self.lbl1 = wx.StaticText(self.box1, -1, "NIVEL DE DIFICULTAD" , pos =(285,50))
-        self.btn1 = wx.Button(self.box1, label = "FACIL", size=(200, 30), pos=(250, 100))
-        self.btn2 = wx.Button(self.box1, label = "NORMAL", size=(200, 30), pos=(250, 150))
-        self.btn3 = wx.Button(self.box1, label = "DIFICIL", size=(200, 30), pos=(250, 200))
+        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(10,0),size=(310, 300))
+        self.lbl1 = wx.StaticText(self.box1, -1, "NIVEL DE DIFICULTAD" , pos =(100,50))
+        self.btn1 = wx.Button(self.box1, label = "FACIL", size=(200, 50), pos=(50, 100))
+        self.btn2 = wx.Button(self.box1, label = "NORMAL", size=(200, 50), pos=(50, 170))
+        self.btn3 = wx.Button(self.box1, label = "DIFICIL", size=(200, 50), pos=(50, 240))
+        
+        #Color Letra Botones
+        self.btn1.SetForegroundColour((3, 152, 252, 255))
+        self.btn2.SetForegroundColour((3, 152, 252, 255))
+        self.btn3.SetForegroundColour((3, 152, 252, 255))
         
         # Eventos de Botones
         #self.btn1.Bind(wx.EVT_BUTTON, self.NivelFacil)
