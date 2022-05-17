9fd6a489d2cc9df9ec1398def241f082cbd19c58
NicKoAz <nicobk.carreno@gmail.com>
2022-05-17 00:34:33 -0400

    añadir archivos

---
+++ modelo/dificultad.py
@@ -0,0 +1,8 @@
+'''
+Created on 17 may 2022
+
+@author: Nicolas
+'''
+class dificultad:
+    def __init__ (self, facil, normal, dificil):
+        pass
--- modelo/modelo.py
+++ modelo/modelo.py
@@ -1,2 +1,8 @@
-Class Modelo:
-pass
+'''
+Created on 17 may 2022
+
+@author: Nicolas
+'''
+class Pieza:
+    listaFiguras =["leon","platano","naranja","elefante","manzana","perro", "gato"]
+    pass
