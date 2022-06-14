8f87f880e954c91f6e903e7be5df84d9c8f531a1
DonCaut <elpolodog@gmail.com>
2022-06-13 20:20:15 -0400

    Actualizada

--- vista/GrillaTest.py
+++ vista/GrillaTest.py
@@ -2,28 +2,37 @@ import wx
 import random
 
 
+def GetLabel(event):
+    nombreboton=event.GetEventObject().GetLabel()
+    print(nombreboton)
+    
 
+
+def TestNombre(event):
+    print("hola")
+    
 filas=4
 columnas=4
-
+#'''MyPanel: Clase  que contiene  a la grilla'''
 class MyPanel(wx.Panel):
     def __init__(self,parent):
         super(MyPanel,self).__init__(parent)
-        
+#'''Randomizador de la grilla'''        
         rdm=random.sample(range(1,30),8)
         rdm.extend(rdm)
         random.shuffle(rdm)
         print(rdm)
-        
+       
         gridsizer=wx.GridSizer(filas,columnas,3,3)
         #for i in range(1,17):
         for i in (rdm):
             #btn="btn"+ str(i)
             btn=str(i)
-            gridsizer.Add(wx.Button(self, label=btn),0,wx.EXPAND)
+            gridsizer.Add(wx.Button(self, label=btn,name=btn),0,wx.EXPAND)
             self.SetSizer(gridsizer)
+            self.Bind( wx.EVT_BUTTON,GetLabel)
             
-            
+    
         
 
 class MyFrame(wx.Frame):
@@ -38,7 +47,8 @@ if __name__=="__main__" :
     app=wx.App()
     frame=MyFrame(parent=None,id=-1)
     frame.Show()
-    app.MainLoop()       
+    app.MainLoop()      
     
     
+
 
