c9016a7fcccd65856d77b90c5c0b72224db81479
Carolina <carolina.hp251014@gmail.com>
2022-06-17 19:24:55 -0400

    Agregar codigo a Main

--- vista/Main.py
+++ vista/Main.py
@@ -1,2 +1,10 @@
 class Main():
     pass
+
+import wx
+from vista.VentanaPrincipal import VentanaPrincipal
+
+if __name__ == "__main__":
+    app = wx.App()
+    VentanaPrincipal().Show()
+    app.MainLoop()
