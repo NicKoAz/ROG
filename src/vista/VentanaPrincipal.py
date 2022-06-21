3e1558120e0299687e570ec2d1206069cabe1e44
Az <Az@Az-Desktop>
2022-06-21 16:23:49 -0400

    actualizacion

--- src/vista/VentanaPrincipal.py
+++ src/vista/VentanaPrincipal.py
@@ -1,68 +1,57 @@
 '''
-Created on 16-05-2022
+Created on 21 jun 2022
 
-@author: carol
+@author: Nico
 '''
-
 import wx
-from vista.VentanaModoJuego import VentanaModoDeJuego
+from vista.VentanaModosDeJuego import VentanaModosDeJuego
 
-'''
-    Esto es un documento  
-'''
-
-'''
-    Esto es una clase
-'''
 
 class VentanaPrincipal(wx.Frame):
-    '''
-    Este metodo es el __init__, el constructor de la clase VentanaPrincipal
-    '''
+
     def __init__(self):
-        wx.Frame.__init__(self, None, title="Encontrar Los Pares", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(350, 350))
+        wx.Frame.__init__(self, None, title="Encontrar Los Pares", style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX),size=(568, 400))
         
         self.panel = wx.Panel(self)
         
-        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(0,-7),size=(350, 350))
-        self.lbl1 = wx.StaticText(self.box1,-1, 'ENCONTRAR LOS PARES', pos =(63,75))
-        self.btn1 = wx.Button(self.box1, label = "INICIAR EL JUEGO", size=(150, 50), pos=(93, 125))
-        self.btn2 = wx.Button(self.box1, label = "SALIR DEL JUEGO", size=(150, 50), pos=(93, 195))
+        self.box1 = wx.StaticBox(self.panel, wx.ID_ANY, pos=(0,-7),size=(568, 400))
         
-        #Modificar Fuente
+        self.lbl1 = wx.StaticText(self.box1,-1, 'ENCONTRAR LOS PARES', pos =(132,60))
+        self.btniniciarjuego = wx.Button(self.box1, label = "INICIAR EL JUEGO", size=(165, 60), pos=(190, 135))
+        self.btnsalirjuego = wx.Button(self.box1, label = "SALIR DEL JUEGO", size=(165, 60), pos=(190, 220))
+        
+        #Modificar fuente de letra StaticText
         font1=self.lbl1.GetFont()
-        font1.SetPointSize(15)
-        self.lbl1.SetFont(font1)        
+        font1.SetPointSize(20)
+        self.lbl1.SetFont(font1)
         
-        #Color fondo stacticBox
+        #Modificar fuente de letra Button Iniciar Juego
+        font1=self.btniniciarjuego.GetFont()
+        font1.SetPointSize(12)
+        self.btniniciarjuego.SetFont(font1) 
+        
+        #Modificar fuente de letra Button Salir Juego
+        font1=self.btnsalirjuego.GetFont()
+        font1.SetPointSize(12)
+        self.btnsalirjuego.SetFont(font1)       
+        
+        #Modificar color de fondo del StaticBox
         self.box1.SetBackgroundColour("#B9D9D7")
         
-        # Eventos de Botones
-        self.btn1.Bind(wx.EVT_BUTTON, self.StarGame)
-        self.btn2.Bind(wx.EVT_BUTTON, self.FinishGame)
+        #Eventos de Botones
+        self.btniniciarjuego.Bind(wx.EVT_BUTTON, self.InicioJuego)
+        self.btnsalirjuego.Bind(wx.EVT_BUTTON, self.FinDelJuego)
         
+        #Para que la ventana se abra en el centro de la pantalla
         self.Centre(True)
+
         
-    def StarGame(self,i):
-        '''
-        Este metodo es para darle una funcion al boton "iniciar juego"
-    
-        :i: es un evento que sirve para abrir una ventana
-        '''
+    def InicioJuego(self,i):
         self.Hide()
-        ventanaMode=VentanaModoDeJuego(self)
+        ventanaMode=VentanaModosDeJuego(self)
         ventanaMode.ShowModal()
         ventanaMode.Destroy()
     
-    
-    def FinishGame(self,i):
-        '''
-        Este metodo es para darle una funcion al boton "Salir del juego"
-    
-        :i: es un evento que sirve para cerrar la ventana
-        '''
+    def FinDelJuego(self,i):
         VentanaPrincipal()
         self.Destroy()
-
-
-    
