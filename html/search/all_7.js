c37d0216c3af267378bb845727ac7c4629550f5b
Carolina <carolina.hp251014@gmail.com>
2022-06-22 02:20:44 -0400

    documentacion

---
+++ html/search/all_7.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['main_12',['Main',['../namespacevista_1_1_main.html',1,'vista']]],
+  ['ventanajuego_13',['VentanaJuego',['../classvista_1_1_ventana_juego_1_1_ventana_juego.html',1,'vista.VentanaJuego.VentanaJuego'],['../namespacevista_1_1_ventana_juego.html',1,'vista.VentanaJuego']]],
+  ['ventanamensajeganador_14',['VentanaMensajeGanador',['../classvista_1_1_ventana_mensaje_ganador_1_1_ventana_mensaje_ganador.html',1,'vista.VentanaMensajeGanador.VentanaMensajeGanador'],['../namespacevista_1_1_ventana_mensaje_ganador.html',1,'vista.VentanaMensajeGanador']]],
+  ['ventanamensajeperdedor_15',['VentanaMensajePerdedor',['../classvista_1_1_ventana_mensaje_perdedor_1_1_ventana_mensaje_perdedor.html',1,'vista.VentanaMensajePerdedor.VentanaMensajePerdedor'],['../namespacevista_1_1_ventana_mensaje_perdedor.html',1,'vista.VentanaMensajePerdedor']]],
+  ['ventanamodosdejuego_16',['VentanaModosDeJuego',['../classvista_1_1_ventana_modos_de_juego_1_1_ventana_modos_de_juego.html',1,'vista.VentanaModosDeJuego.VentanaModosDeJuego'],['../namespacevista_1_1_ventana_modos_de_juego.html',1,'vista.VentanaModosDeJuego']]],
+  ['ventanaprincipal_17',['VentanaPrincipal',['../classvista_1_1_ventana_principal_1_1_ventana_principal.html',1,'vista.VentanaPrincipal.VentanaPrincipal'],['../namespacevista_1_1_ventana_principal.html',1,'vista.VentanaPrincipal']]]
+];
